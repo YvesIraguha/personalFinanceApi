1d5d940ae485b2be891516c7b598f3366c2cb649
YvesIraguha <iraguhaivos@gmail.com>
2019-07-31 13:05:19 +0200

    chore(fix): enable editing expense - enable editing an expense - enable deleting an expense [#167564572]

--- src/api/resolvers.js
+++ src/api/resolvers.js
@@ -1,20 +1,35 @@
 const resolvers = {
-    Query:{
-        async getAllExpenses(root,args,{models}){
-            return models.Expense.findAll()
-        },
-        async  getExpense(root,{id},{models}){
-            return models.Expense.findOne({where:{id}})
-        }
-    
+  Query: {
+    async getAllExpenses(root, args, { models }) {
+      return models.Expense.findAll();
     },
-    Mutation:{
-        async createExpense(root,{type,quantity,price},{models}){
-        return models.Expense.create({
-            type,quantity,price
-        })
-        }
+    async getExpense(root, { id }, { models }) {
+      return models.Expense.findOne({ where: { id } });
     }
-}
+  },
+  Mutation: {
+    async createExpense(root, { type, quantity, price }, { models }) {
+      return models.Expense.create({
+        type,
+        quantity,
+        price
+      });
+    },
+    async deleteExpense(root, { id }, { models }) {
+      const deleted = await models.Expense.destroy({
+        where: { id },
+        returning: true
+      });
+      return deleted;
+    },
+    async updateExpense(root, { id, ...rest }, { models }) {
+      const updated = await models.Expense.update(rest, {
+        where: { id },
+        returning: true
+      });
+      return updated[1][0].dataValues;
+    }
+  }
+};
 
 export default resolvers;
--- src/api/schema.js
+++ src/api/schema.js
@@ -1,20 +1,22 @@
 import { gql } from "apollo-server-express";
 
 const typeDefs = gql`
-type Expense {
-    id:String
-    type:String
-    quantity:Int
-    price:Int
-}
-type Query {
-    getAllExpenses:[Expense!]!
-    getExpense(id:String!):Expense
-}
+  type Expense {
+    id: String
+    type: String
+    quantity: Int
+    price: Int
+  }
+  type Query {
+    getAllExpenses: [Expense!]!
+    getExpense(id: String!): Expense
+  }
 
-type Mutation {
-    createExpense(type:String!,quantity:Int,price:Int!):Expense!
-}
-`
+  type Mutation {
+    createExpense(type: String!, quantity: Int, price: Int!): Expense!
+    deleteExpense(id: String!): Expense
+    updateExpense(id: String!, type: String, quantity: Int, price: Int): Expense
+  }
+`;
 
 export default typeDefs;
