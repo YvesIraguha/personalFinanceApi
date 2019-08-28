b52e76ae68848c58721bf5e5250d888a58f0129a
YvesIraguha <iraguhaivos@gmail.com>
2019-08-28 15:42:02 +0200

    chore(fix): set up testing environment"

---
+++ tests/user.test.js
@@ -0,0 +1,36 @@
+const chai = require("chai");
+
+const expect = chai.expect;
+const url = `http://localhost:4000`;
+const request = require("supertest")(url);
+
+describe("GraphQL", () => {
+  it("Returns user with id = 10", done => {
+    request
+      .post("/graphql")
+      .send({ query: "{ getUser(id: 10) { firstName } }" })
+      .expect(200)
+      .end((err, res) => {
+        // res will contain array with one user
+        if (err) return done(err);
+        res.body.user.should.have.property("id");
+        res.body.user.should.have.property("name");
+        res.body.user.should.have.property("username");
+        res.body.user.should.have.property("email");
+        done();
+      });
+  });
+
+  it("Returns all users", done => {
+    request
+      .post("/graphql")
+      .send({ query: "{ user { id name username email } }" })
+      .expect(200)
+      .end((err, res) => {
+        // res will contain array of all users
+        if (err) return done(err);
+        // assume there are a 100 users in the database
+        res.body.user.should.have.lengthOf(100);
+      });
+  });
+});
